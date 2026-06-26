718e342d985f4851daa9c20c9f9ee71363600f8b
Alexandr <124816797+xopsreyli@users.noreply.github.com>
2026-06-26 15:52:25 +0300

    Set basename for BrowserRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,12 @@ import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
+const basename = import.meta.env.BASE_URL
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
+            <BrowserRouter basename={basename}>
                 <App />
             </BrowserRouter>
         </QueryClientProvider>
